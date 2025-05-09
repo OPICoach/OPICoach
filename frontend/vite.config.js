2d485170278cd135b3c83b25d42e85e7f6f86222
jiwon <ysjjw2003@gmail.com>
2025-05-09 15:40:17 +0900

    feat(fe): fix fronted port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5173, // 원하는 포트 번호로 변경
+    strictPort: true, // 해당 포트를 사용 중이면 에러 발생 (자동 증가 방지)
+    mimeTypes: {
+      "text/javascript": ["js", "jsx"],
+    },
   },
 });

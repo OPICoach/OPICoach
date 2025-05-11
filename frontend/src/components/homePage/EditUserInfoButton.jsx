a5832bd765262a73605375babeb5e4e023bc9d97
jiwon <ysjjw2003@gmail.com>
2025-05-11 14:18:22 +0900

    feat(fe): home page for existing user

---
+++ frontend/src/components/homePage/EditUserInfoButton.jsx
@@ -0,0 +1,10 @@
+const EditUserInfoButton = ({ onEdit }) => (
+  <button
+    className="bg-primary hover:bg-blue-600 transition text-white px-6 py-2 rounded-lg shadow-md text-base font-semibold"
+    onClick={onEdit}
+  >
+    Edit User Information
+  </button>
+);
+
+export default EditUserInfoButton;

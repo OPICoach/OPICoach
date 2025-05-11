72e576253b768c98c95a749dd13f22d05d357ce6
jiwon <ysjjw2003@gmail.com>
2025-05-11 13:32:13 +0900

    fix(fe): fix cursor style and sidebar weight

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -14,6 +14,7 @@ const menus = [
   { name: "Information", icon: infoIcon, path: "/information" },
 ];
 
+// 유저 네임 앞자리만 가져오기
 function getProfileInitial(userName) {
   if (!userName) return "";
   const parts = userName.trim().split(" ");
@@ -28,18 +29,18 @@ const SideBar = ({ userName }) => {
   const initial = getProfileInitial(userName);
 
   return (
-    <aside className="flex flex-col w-60 h-screen bg-neutral">
+    <aside className="flex flex-col w-[250px] h-screen bg-neutral ">
       {/* 로고 */}
       <div className="flex items-center justify-center h-24">
         <img src={sidebarLogo} alt="OPICoach Logo" className="h-10" />
       </div>
       {/* 메뉴 */}
-      <nav className="flex-1 mt-8 mx-1">
+      <nav className="flex-1 mt-8 mx-1 ">
         {menus.map((menu) => (
           <a
             key={menu.name}
             href={menu.path}
-            className="flex items-center my-[10px] mx-[5px] px-4 py-3 text-accent border-[#E5E7EB] rounded-lg hover:bg-white transition"
+            className="flex items-center my-[10px] mx-[5px] px-4 py-3 text-accent border-[#E5E7EB] rounded-lg hover:bg-white transition cursor-pointer"
           >
             <img
               src={menu.icon}
@@ -50,15 +51,15 @@ const SideBar = ({ userName }) => {
           </a>
         ))}
       </nav>
-        {/* 프로필 */}
-      <div className="flex items-center px-5 pb-6">
+      {/* 프로필 */}
+      <div className="flex items-center px-5 pb-6 cursor-default">
         <div className="w-9 h-9 bg-[#4490FB] rounded-full flex items-center justify-center mr-3">
           <span className="text-white text-lg font-bold">{initial}</span>
         </div>
         <span className="text-black font-medium">{userName}</span>
         <button
           className="ml-auto hover:bg-gray-100 rounded-full"
-        //   onClick={() => navigate("/setting")}
+          //   onClick={() => navigate("/setting")}
           title="설정"
         >
           <img src={settingIcon} alt="설정" className="w-5 h-5" />

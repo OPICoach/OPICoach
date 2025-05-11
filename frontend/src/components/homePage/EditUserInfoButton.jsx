ef3369dd84de206836900f0e64b50b38a67c2108
jiwon <ysjjw2003@gmail.com>
2025-05-11 14:39:19 +0900

    fix(fe): fix homeExistUser style

--- frontend/src/components/homePage/EditUserInfoButton.jsx
+++ frontend/src/components/homePage/EditUserInfoButton.jsx
@@ -1,6 +1,6 @@
 const EditUserInfoButton = ({ onEdit }) => (
   <button
-    className="bg-primary hover:bg-blue-600 transition text-white px-6 py-2 rounded-lg shadow-md text-base font-semibold"
+    className="bg-primary hover:bg-blue-600 transition text-white px-6 py-2 rounded-lg text-base font-semibold"
     onClick={onEdit}
   >
     Edit User Information

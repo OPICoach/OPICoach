6924612d77aa89402ecc3cd84aeef7d88f9287a4
jiwon <ysjjw2003@gmail.com>
2025-05-11 11:25:15 +0900

    feat(fe): add sidebar component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import SideBar from "../components/sidebar";
+
 const Home = () => {
-  return <div className="flex">OPICoach</div>;
+  return (
+    <div className="">
+      <SideBar userName=""/>
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Home;

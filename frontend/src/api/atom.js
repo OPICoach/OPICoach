da05c8ff831f883ec9f26da5140cf5d5ef3088f7
jiwon <ysjjw2003@gmail.com>
2025-05-16 15:34:11 +0900

    fix(fe): fix atom

--- frontend/src/api/atom.js
+++ frontend/src/api/atom.js
@@ -1,6 +1,21 @@
 import { atom } from "recoil";
-
 export const userDataState = atom({
   key: "userDataState",
   default: null,
+  effects_UNSTABLE: [
+    ({ setSelf, onSet }) => {
+      // 초기값을 localStorage에서 가져오기
+      const saved = localStorage.getItem("userDataState");
+      if (saved != null) setSelf(JSON.parse(saved));
+
+      // 값이 바뀔 때마다 localStorage에 저장
+      onSet((newValue, _, isReset) => {
+        if (isReset || newValue == null) {
+          localStorage.removeItem("userDataState");
+        } else {
+          localStorage.setItem("userDataState", JSON.stringify(newValue));
+        }
+      });
+    },
+  ],
 });
